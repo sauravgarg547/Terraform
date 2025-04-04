461cd5a211f2eccd11d638e6357ebc84442b5f14
sauravgarg547 <sauravgarg5922@gmail.com>
2025-04-04 21:06:22 +0545

    make ec2

--- ec2.tf
+++ ec2.tf
@@ -1,56 +1,64 @@
 # key pair for ec2
-
 resource "aws_key_pair" "my-key" {
-  key_name = "terra-key"
+  key_name   = "terra-key"
   public_key = file("terra-key.pub")
 }
 
-# vpc for ec2
-
+# VPC for EC2
 resource "aws_default_vpc" "default" {
   tags = {
     Name = "Default VPC"
   }
 }
 
-# Aws Security Group
-
+# AWS Security Group
 resource "aws_security_group" "my_sg" {
-    name = "mysg-ec2"
-    description = "Allow TLS inbound traffic and all outbound traffic"
-    vpc_id = aws_default_vpc.default
+  name        = "mysg-ec2"
+  description = "Allow SSH and HTTPS traffic"
+  vpc_id      = aws_default_vpc.default.id
 
-    #inboud rule
+  # ✅ Corrected Ingress Rule
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 
-    ingress = {
-        from_port = 22
-        to_port = 22
-        protocol = "tcp"
-        cidr_blocks = ["0.0.0.0/0"]
-    }
+  ingress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  # ✅ Corrected Egress Rule
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "My Security Group"
+  }
 }
-resource "aws_vpc_security_group_ingress_rule" "allow_tls_ipv4" {
-  security_group_id = aws_security_group.my_sg
-  cidr_ipv4         = aws_vpc.main.cidr_block
-  from_port         = 443
-  ip_protocol       = "tcp"
-  to_port           = 443
-  cidr_ipv6         = aws_default_vpc.ipv6_cidr_block
-}
 
-resource "aws_vpc_security_group_egress_rule" "my_outbond" {
-  security_group_id = aws_security_group.my_sg
-  from_port = 0
-  to_port = 0
-  ip_protocol = "-1"
-  cidr_ipv4 = "::/0"
-}
 
-# aws ec2 instances
-
+# AWS EC2 Instance
 resource "aws_instance" "my_ec2" {
-  key_name = aws_key_pair.my-key.key_name
-  instance_type = "t2.micro"
-  security_groups = aws_security_group.my_sg.name
-  ami = "ami-0e35ddab05955cf57"
+  ami                    = "ami-0e35ddab05955cf57"
+  instance_type          = "t2.micro"
+  key_name               = aws_key_pair.my-key.key_name
+  vpc_security_group_ids = [aws_security_group.my_sg.id]
+
+  root_block_device {
+    volume_size = 15
+    volume_type = "gp3"
+  }
+
+  tags = {
+    Name = "maccotech-aws"
+  }
 }
--- s3.tf
+++ s3.tf
@@ -1,4 +1,4 @@
-resource "aws_s3_bucket" "my_s3" {
-    bucket = "mys3maccotech"
-}
+# resource "aws_s3_bucket" "my_s3" {
+#     bucket = "mys3maccotech"
+# }
 
